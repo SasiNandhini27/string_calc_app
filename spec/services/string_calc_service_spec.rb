76dc55700989956913714e6bf68ee24f03213cbf
Sasi Nandhini <nandhu9655@gmail.com>
2025-04-04 19:50:54 +0530

    covered test cases for accepting n numbers

--- spec/services/string_calc_service_spec.rb
+++ spec/services/string_calc_service_spec.rb
@@ -8,5 +8,17 @@ RSpec.describe StringCalculatorService, type: :service do
         it 'returns 0 for an empty string' do
             expect(StringCalculatorService.add('')).to eq(0)
         end
+        
+        it 'returns the sum of single number' do
+            expect(StringCalculatorService.add('3')).to eq(3)
+        end
+        
+        it 'returns the sum of two numbers' do
+            expect(StringCalculatorService.add('7,3')).to eq(10)
+        end
+
+        it 'returns the sum of five numbers' do
+            expect(StringCalculatorService.add('2,5,4,15,3,1')).to eq(30)
+        end
     end
 end
--- string_calc_service.rb
+++ string_calc_service.rb
@@ -2,7 +2,8 @@
 
 class StringCalculatorService
     def self.add(numbers)
-        
+        return 0 if numbers.empty?
+        numbers.split(',').map(&:to_i).sum
     end
 end
   
